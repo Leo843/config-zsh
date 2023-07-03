afc27ee29607b52f7f0a8e6d03950a9d9baf73b0
Leo Carpenter <leo.carpenter.pro@gmail.com>
2023-07-03 18:36:31 +0200

    remove unused aliases

--- alias.zsh
+++ alias.zsh
@@ -26,41 +26,6 @@ function cd () {
   builtin cd "$@" && ll
 }
 
-# convert a plantuml file to svg
-function pu2svg () {
-  cat $1 | docker run --rm -i think/plantuml
-}
-
-# run http-server in current dicrectory
-# - option `-p 8081` to use port 8081 instead of 8080 (default)
-# - option `-c-1` to disable caching
-alias http-server='docker run --rm -it -p 8081:8081 -v $(pwd):/public danjellz/http-server http-server -p 8081 -c-1'
-
-# run grip in current directory
-alias markdown-server='docker run --rm -it -p 8082:8082 -v $(pwd):/data --user root mbentley/grip 0.0.0.0:8082'
-
-# connect to hammer with several ports forwarded
-alias ssh-hammer='ssh -i ~/.ssh/hammer \
-  -L 8080:hammer:8080 \
-  -L 8081:hammer:8081 \
-  -L 8082:hammer:8082 \
-  -L 8083:hammer:8083 \
-  -L 8084:hammer:8084 \
-  -L 4200:hammer:4200 \
-  -L 4201:hammer:4201 \
-  -L 4202:hammer:4202 \
-  -L 4203:hammer:4203 \
-  -L 4204:hammer:4204 \
-  -L 3000:hammer:3000 \
-  -L 3001:hammer:3001 \
-  -L 3002:hammer:3002 \
-  -L 3003:hammer:3003 \
-  -L 3004:hammer:3004 \
-  leo@hammer'
-
-# WARNING: The 'sed' version from busybox does not behave well with the script.
-alias kcomplete='source <(kubectl completion zsh)'
-
 alias k='kubectl'
 
 alias tf='terraform'
